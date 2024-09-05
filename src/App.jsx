ac433bab0ef0cd9d79e5f2344b47d5bc091eb93d
swami1302 <swamii1413@gmail.com>
2024-09-05 09:46:42 +0530

    Cards modified and Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,13 @@ import About from './components/About'
 import Eyes from './components/Eyes'
 import Features from './components/Features'
 import Cards from './components/Cards'
+import Footer from './components/Footer'
+import LocomotiveScroll from 'locomotive-scroll';
 
 function App() {
+
+const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <div className='w-full min-h-screen text-white bg-zinc-900'>
       <Navbar/>
@@ -17,6 +22,7 @@ function App() {
       <Eyes/>
       <Features/>
       <Cards/>
+      <Footer/>
 
     </div>
   )
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,11 +4,20 @@ function Cards() {
   return (
     <div className='w-full h-screen bg-zinc-900 flex items-center px-32 gap-5'>
         <div className="cardContainer h-[50vh] w-1/2 ">
-            <div className="card w-full h-full bg-[#004D43] rounded-xl"></div>
+            <div className="card w-full h-full bg-[#004D43] rounded-xl flex items-center justify-center relative">
+              <img className='w-32' src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg" alt="" />
+              <button className='absolute bottom-10 left-10 px-5 py-1 border-2 rounded-full text-white text-sm'>&copy;2019 -2020 </button>
+            </div>
         </div>
         <div className="cardContainer flex gap-5 h-[50vh] w-1/2">
-            <div className="card w-1/2 h-full bg-[#274743] rounded-xl"></div>
-            <div className="card w-1/2 h-full bg-[#274743] rounded-xl"></div>
+            <div className="card w-1/2 h-full bg-[#212121] rounded-xl flex items-center justify-center relative">
+              <img className='w-32' src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg" alt="" />
+              <button className='absolute bottom-10 left-10 px-5 py-1 border-2 rounded-full text-white text-sm uppercase'>rating 5.0 on clutch </button>
+            </div>
+            <div className="card w-1/2 h-full bg-[#212121] rounded-xl flex items-center justify-center relative">
+              <img className='w-32' src="https://ochi.design/wp-content/uploads/2022/04/logo003.png" alt="" />
+              <button className='absolute bottom-10 left-10 px-5 py-1 border-2 rounded-full text-white text-sm uppercase'>Business Bootcamp Alumni</button>
+            </div>
         </div>
     
     </div>
